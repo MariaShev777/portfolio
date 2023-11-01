6cf126f454d0f4bc5c86d5c774855ff651a5e30f
MariaShev777 <masha_shevtsova@tut.by>
2023-11-01 15:55:32 +0300

    styled Header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import {Menu} from "../../components/menu/Menu";
 import {Icon} from "../../components/icon/Icon";
 import styled from "styled-components";
 import {Container} from "../../components/Container";
 import {FlexWrapper} from "../../components/FlexWrapper";
+import {HeaderMenu} from "./header-menu/HeaderMenu";
 
 const items = ["Home", "Skills", "Projects", "Contacts"];
 
@@ -13,7 +13,7 @@ export const Header = () => {
             <Container>
                 <FlexWrapper justify={'space-between'} align={'center'}>
                     <Icon iconId={"logo"}/>
-                    <Menu menuItems={items}/>
+                    <HeaderMenu menuItems={items}/>
                 </FlexWrapper>
             </Container>
         </StyledHeader>
@@ -22,11 +22,12 @@ export const Header = () => {
 
 
 const StyledHeader = styled.header`
-  background-color: #f8f8b1;
-  //padding: 0 20px;
-  //position: fixed;
-  //width: 100%;
-  //top: 0;
-  //left: 0;
+  background-color: rgba(31, 31, 32, 0.9);
+  padding: 20px 0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 999;
   //box-shadow: 0 0 40px rgba(0, 0, 0, 0.26), 0 0 2px rgba(0, 0, 0, 0.1);
 `
