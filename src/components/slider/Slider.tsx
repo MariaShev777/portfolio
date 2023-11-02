b33dd3d5805632db794b4d65f6e48900a6bb9397
MariaShev777 <masha_shevtsova@tut.by>
2023-11-02 12:21:24 +0300

    styled Testimony

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import {FlexWrapper} from "../FlexWrapper";
+import {theme} from "../../styles/theme";
 
 export const Slider = () => {
     return (
@@ -15,7 +16,7 @@ export const Slider = () => {
             </FlexWrapper>
             <Pagination>
                 <span> </span>
-                <span> </span>
+                <span className={'active'}> </span>
                 <span> </span>
             </Pagination>
         </StyledSlider>
@@ -24,7 +25,6 @@ export const Slider = () => {
 
 
 const StyledSlider = styled.div`
-  border: 1px solid red;
   max-width: 500px;
   display: flex;
   flex-direction: column;
@@ -33,7 +33,6 @@ const StyledSlider = styled.div`
 `
 
 const Slide = styled.div`
-  background-color: rgba(255, 255, 255, 0.51);
   text-align: center;
 `
 
@@ -42,16 +41,30 @@ const Text = styled.p`
 `
 
 const Name = styled.span`
-
+  font-family: 'Josefin Sans', sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  display: inline-block;
+  margin: 22px 0 42px;
 `
 
 const Pagination = styled.div`
   span {
     display: inline-block;
-    width: 20px;
-    height: 20px;
-    margin: 5px;
-    border-radius: 50%;
-    background-color: #2a2a2c;
+    width: 7px;
+    height: 7px;
+    border-radius: 20px;
+    background-color: rgba(255, 255, 255, 0.5);
+    
+    & + span {
+      margin-left: 5px;
+    }
+    
+    &.active {
+      background-color: ${theme.colors.accent};
+      width: 20px;
+    }
   }
 `
--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -4,13 +4,16 @@ import {SectionTitle} from "../../../components/SectionTitle";
 import {Icon} from "../../../components/icon/Icon";
 import {Slider} from "../../../components/slider/Slider";
 import {FlexWrapper} from "../../../components/FlexWrapper";
+import {IconWrapper} from "../skills/skill/Skill";
 
 export const Testimony = () => {
     return (
         <StyledTestimony>
             <SectionTitle>Testimony</SectionTitle>
-            <FlexWrapper direction={'column'} align={'center'}>
-                <Icon iconId={"logo"}/>
+            <FlexWrapper direction={"column"} align={"center"}>
+                <IconWrapper>
+                    <Icon iconId={"logo"}/>
+                </IconWrapper>
                 <Slider/>
             </FlexWrapper>
         </StyledTestimony>
@@ -19,6 +22,10 @@ export const Testimony = () => {
 
 
 const StyledTestimony = styled.section`
-  //background-color: #b47df5;
   min-height: 50vh;
+  
+  ${IconWrapper} {
+  margin: 40px 0 72px;
+}
 `
+
