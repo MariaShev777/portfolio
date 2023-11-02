4c1110f5b781ba2611463fc1d1372c98fa5f675d
MariaShev777 <masha_shevtsova@tut.by>
2023-11-02 13:35:07 +0300

    styled Contacts

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -2,36 +2,64 @@ import React from "react";
 import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle";
 import {StyledButton} from "../../../components/Button";
+import { Container } from "../../../components/Container";
+import {theme} from "../../../styles/theme";
 
 export const Contacts = () => {
     return (
         <StyledContacts>
-            <SectionTitle>Contacts</SectionTitle>
-            <StyledForm>
-                <Field placeholder='Your e-mail'></Field>
-                <Field placeholder='Subject'></Field>
-                <Field placeholder='Type here...' as={'textarea'}></Field>
-                <StyledButton type='submit'>Send</StyledButton>
-            </StyledForm>
+            <Container>
+                <SectionTitle>Contacts</SectionTitle>
+                <StyledForm>
+                    <Field placeholder='Your e-mail'></Field>
+                    <Field placeholder='Subject'></Field>
+                    <Field placeholder='Type here...' as={'textarea'}></Field>
+                    <StyledButton type='submit'>Send</StyledButton>
+                </StyledForm>
+            </Container>
         </StyledContacts>
     );
 };
 
 const StyledContacts = styled.section`
-  min-height: 50vh;
-  background-color: #331616;
+  
 `
 
 
 const StyledForm = styled.form`
-  background-color: rgba(255, 255, 255, 0.44);
-  max-width: 500px;
+  max-width: 540px;
   width: 100%;
   display: flex;
   flex-direction: column;
-  gap: 10px;
+  align-items: center;
+  gap: 16px;
   margin: 0 auto;
+  
+  textarea {
+    resize: none;
+    height: 155px;
+  }
 `
 
 const Field = styled.input`
+  width: 100%;
+  background-color: ${theme.colors.secondaryBg};
+  border: 1px solid ${theme.colors.borderColor};
+  padding: 7px 15px;
+  
+  font-family: 'Poppins', sans-serif;
+  font-weight: 400;
+  font-size: 12px;
+  letter-spacing: 0.05em;
+  
+  color: ${theme.colors.font};
+  
+  &::placeholder {
+    color: ${theme.colors.placeholderColor};
+  }
+
+  &:focus-visible {
+    outline: 1px solid ${theme.colors.borderColor};
+  }
+  
 `
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,6 +4,10 @@ export const theme = {
         secondaryBg: '#252527',
         accent: '#ffff00',
 
-        font: '#FFFFFF'
+        font: '#FFFFFF',
+
+        borderColor: '#4A4A4A',
+
+        placeholderColor: '#495057'
     }
 }
